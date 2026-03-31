1b87b7372e744493b1c026d15bfe1e04686524e0
arslannaseer346 <arslannaseer346@gmail.com>
2026-03-31 23:52:30 +0500

    UI completed right side

--- 01 React project uI/src/components/section2/Lefttext.jsx
+++ 01 React project uI/src/components/section2/Lefttext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Lefttext = () => {
   return (
-    <div className='w-1/4 h-100 mt-8 mx-3 relative flex flex-col place-content-between'>
+    <div className='w-1/4 h-120 mt-8 mx-3 relative flex flex-col justify-between'>
       <div >
       <h3 className='text-5xl  px-4 font-bold mb-3'>Prospective customer segmentation</h3>
       <p className='px-4 py-1 font-extralight text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt illum laboriosam quo incidunt asperiores, laborum hic porro, tenetur eos explicabo consectetur aperiam </p>
--- 01 React project uI/src/components/section2/Rightcard.jsx
+++ 01 React project uI/src/components/section2/Rightcard.jsx
@@ -1,25 +1,13 @@
 import React from 'react'
-
+import Righcardtcontent from './Righcardtcontent'
 const Rightcard = () => {
   return (
-   <div className='w-3/5 relative mt-8  mx-1 max-h'>
-   <div >
-    <img className=' rounded-3xl object-fit h-100 w-60' src="https://i.pinimg.com/736x/1b/4d/df/1b4ddf8afc6c280795f0607148fefee9.jpg"  alt="" />
-   <div className=' absolute bottom-0 top-0 w-60 text-white flex flex-col place-content-between'>
-    <div>
-
-    <h3 className=' bg-gray-200 rounded-full content-center items-center w-8 h-10' >     1</h3>
-    </div>
-    <div>
-        <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto reiciendis necessitatibus at eum perspiciatis voluptatum vero sunt odit quaerat vitae quod sit laudantium provident, explicabo tenetur cumque, quidem illum praesentium.
-        </h4>
-        <div>
-
-        <button>Satisfied</button>
-        </div>
-    </div>
-   </div>
-   </div>
+   <div className='w-3/5 overflow-x-auto gap-6 custom-scroll flex flex-nowrap mt-8  mx-1 h-115'>
+      <Righcardtcontent />
+      <Righcardtcontent />
+      <Righcardtcontent />
+      <Righcardtcontent />
+      <Righcardtcontent />
    </div>
   )
 }
