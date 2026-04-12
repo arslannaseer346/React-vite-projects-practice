5eac5f74871c025613b557889626cec597a4b804
arslannaseer346 <arslannaseer346@gmail.com>
2026-04-12 22:23:39 +0500

    ui creating notes

---
+++ 03 Notes App project using React + vite/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className=' flex w-full h-screen' >
+        <div className=' w-1/2 bg-amber-800'>
+          <h1>ADD NOTES</h1>
+          <form>
+
+          </form>
+        </div>
+        <div className='w-1/2 bg-amber-300'>Recent Notes</div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
