de2b57df28352f88989e4094da132128a58310aa
arslannaseer346 <arslannaseer346@gmail.com>
2026-04-17 22:30:29 +0500

    form prevent default

--- 03 Notes App project using React + vite/src/App.jsx
+++ 03 Notes App project using React + vite/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 const App = () => {
+  const SubmitHandler= (e)=>{
+    e.preventDefault()
+    console.log("form submited");
+    
+  }
   return (
     <div className='bg-black h-screen w-full text-white'>
-        <form className=' flex justify-between items-start p-10'>
+        <form onSubmit={(e)=>{SubmitHandler(e)}} className=' flex justify-between items-start p-10'>
           <div className='w-1/2 flex flex-col items-start gap-4'>
-          <input type="text" className='w-full outline-none px-5 py-2 rounded border-2' placeholder='Enter Notes Title' />
-          <textarea  className='w-full outline-none h-32 flex items-start px-5 py-2 rounded  border-2' placeholder='Enter Details ' id=""></textarea>
-          <button className= 'w-full bg-white  outline-none text-black px-5 py-2 rounded '>Add Notes</button>
+          <input type="text" className='w-full font-medium outline-none px-5 py-2 rounded border-2' placeholder='Enter Notes Title' />
+          <textarea  className='w-full font-medium outline-none h-32 flex items-start px-5 py-2 rounded  border-2' placeholder='Enter Details ' id=""></textarea>
+          <button className= 'w-full bg-white font-medium outline-none text-black px-5 py-2 rounded '>Add Notes</button>
           </div>
           
         </form>
