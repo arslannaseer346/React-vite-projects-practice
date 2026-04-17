8db2bba00a99573a0ac98ea5b579e1eaace5fc3e
arslannaseer346 <arslannaseer346@gmail.com>
2026-04-17 23:38:42 +0500

    two way binding

--- 03 Notes App project using React + vite/src/App.jsx
+++ 03 Notes App project using React + vite/src/App.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
   const SubmitHandler= (e)=>{
     e.preventDefault()
     console.log("form submited");
     console.log(title);
+    console.log(details);
     
   }
   return (
@@ -13,7 +15,7 @@ const App = () => {
         <form onSubmit={(e)=>{SubmitHandler(e)}} className='gap-4 p-10 flex-col flex lg:w-1/2 items-start '>
           <h1 className=' text-4xl font-bold'>Add Notes</h1>
           <input value={title} onChange={(e)=>{setTitle(e.target.value)}} type="text" className='w-full font-medium outline-none px-5 py-2 rounded border-2' placeholder='Enter Notes Title' />
-          <textarea  className='w-full font-medium outline-none h-32 flex items-start px-5 py-2 rounded  border-2' placeholder='Enter Details ' id=""></textarea>
+          <textarea value={details} onChange={(e)=>{setDetails(e.target.value)}} className='w-full font-medium outline-none h-32 flex items-start px-5 py-2 rounded  border-2' placeholder='Enter Details ' id=""></textarea>
           <button className= 'w-full bg-white font-medium outline-none text-black px-5 py-2 rounded '>Add Notes</button>
         </form>
         <div className=' lg:border-l-2 lg:w-1/2 p-10'>
