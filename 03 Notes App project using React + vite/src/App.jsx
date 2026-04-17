613a67944697459aa8f88eda5385ca10af3cd34f
arslannaseer346 <arslannaseer346@gmail.com>
2026-04-17 15:06:38 +0500

    notes structure created

--- 03 Notes App project using React + vite/src/App.jsx
+++ 03 Notes App project using React + vite/src/App.jsx
@@ -2,17 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <div className=' flex w-full h-screen' >
-        <div className=' w-1/2 bg-amber-800'>
-          <h1>ADD NOTES</h1>
-          <form>
-
-          </form>
-        </div>
-        <div className='w-1/2 bg-amber-300'>Recent Notes</div>
-      </div>
-
+    <div className='bg-black h-screen w-full text-white'>
+        <form className=' flex justify-between items-start p-10'>
+          <div className='w-1/2 flex flex-col items-start gap-4'>
+          <input type="text" className='w-full outline-none px-5 py-2 rounded border-2' placeholder='Enter Notes Title' />
+          <textarea  className='w-full outline-none h-32 flex items-start px-5 py-2 rounded  border-2' placeholder='Enter Details ' id=""></textarea>
+          <button className= 'w-full bg-white  outline-none text-black px-5 py-2 rounded '>Add Notes</button>
+          </div>
+          
+        </form>
     </div>
   )
 }
