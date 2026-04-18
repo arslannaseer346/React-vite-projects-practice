938e43c21f538f56d38cf1bf5ccda048064bbc49
arslannaseer346 <arslannaseer346@gmail.com>
2026-04-18 14:10:23 +0500

    storing data into list

--- 03 Notes App project using React + vite/src/App.jsx
+++ 03 Notes App project using React + vite/src/App.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react'
 const App = () => {
   const [title, setTitle] = useState('');
   const [details, setDetails] = useState('');
+  const [task, setTask] = useState([]);
   const SubmitHandler= (e)=>{
     e.preventDefault()
     console.log("form submited");
     console.log(title);
     console.log(details);
-    
+    setTitle('')
+    setDetails('')
   }
   return (
     <div className='bg-black h-screen w-full lg:flex text-white gap-5 '>
@@ -16,7 +18,7 @@ const App = () => {
           <h1 className=' text-4xl font-bold'>Add Notes</h1>
           <input value={title} onChange={(e)=>{setTitle(e.target.value)}} type="text" className='w-full font-medium outline-none px-5 py-2 rounded border-2' placeholder='Enter Notes Title' />
           <textarea value={details} onChange={(e)=>{setDetails(e.target.value)}} className='w-full font-medium outline-none h-32 flex items-start px-5 py-2 rounded  border-2' placeholder='Enter Details ' id=""></textarea>
-          <button className= 'w-full bg-white font-medium outline-none text-black px-5 py-2 rounded '>Add Notes</button>
+          <button className= 'w-full active:scale-95 bg-white font-medium outline-none text-black px-5 py-2 rounded '>Add Notes</button>
         </form>
         <div className=' lg:border-l-2 lg:w-1/2 p-10'>
         <h1 className=' text-4xl font-bold'>Recent Notes</h1>
